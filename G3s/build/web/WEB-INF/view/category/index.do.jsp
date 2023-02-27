cfa07d34cad1e1357e6a40ffd6f0b14a81bec02b
John Smith <bao1322002@gmail.com>
2023-02-27 14:01:10 +0700

    Add full enity class, Replace product folder by watch folder,  Repleace ProductController by WatchesController, Add more front

---
+++ G3s/build/web/WEB-INF/view/category/index.do.jsp
@@ -0,0 +1,50 @@
+<%-- 
+    Document   : product
+    Created on : Feb 23, 2023, 9:28:15 AM
+    Author     : duyba
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix ="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<div class="show-product container">
+
+    <table>
+        <c:forEach var="product" items="${listPaging}" varStatus="loop">
+            <c:if test="${loop.index % 3 == 0}">
+                <tr>
+                </c:if>
+                <td>
+                    <div class="show-product-box">
+                        <h3>${product.name}</h3>
+                        <img src="<c:url value="/img/${product.image}" />">
+                        </br>
+                        <p class="price"><fmt:formatNumber value="${product.price}" type = "currency" /></p>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+
+                        <div class="show-product-box-btn">
+                            <a href="#" class="show-product-box-btn-buy">Buy now</a>
+                            <a href="#" class="show-product-box-btn-add">Add to card</a>
+                        </div>
+                    </div>
+                </td>
+                <c:if test="${(loop.index + 1) % 3 == 0 || loop.last}">
+                </tr>
+            </c:if>
+        </c:forEach>
+    </table>
+    <c:forEach begin = "1" end = "${endPage}" var = "i">
+        <a class="page-index" href="<c:url value ="/category/index.do?index=${i}" />">${i}</a>
+    </c:forEach>
+</div>
+
+
+
+
---
+++ G3s/src/java/dal/orderheaderFacade.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import model.customer;
+import model.orderheader;
+
+/**
+ *
+ * @author admin
+ */
+public class orderheaderFacade {
+
+    public List<orderheader> select() throws SQLException {
+        List<orderheader> list = null;
+        //Tạo connection để kết nối vào DBMS
+        Connection con = DBContext.getConnection();
+        //Tạo đối tượng statement
+        Statement stm = con.createStatement();
+        //Thực thi lệnh SELECT
+        ResultSet rs = stm.executeQuery("select * from OrderHeader");
+        list = new ArrayList<>();
+        while (rs.next()) {
+            //Doc mau tin hien hanh de vao doi tuong 
+            orderheader orh = new orderheader();
+            orh.setId(rs.getInt("id"));
+            orh.setDate(rs.getDate("date"));
+            orh.setStatus(rs.getString("status"));
+            orh.setCustomerId(rs.getInt("customerId"));
+            //Them cate vao list
+            list.add(orh);
+        }
+        con.close();
+        return list;
+    }
+
+    public void create(orderheader orh) throws SQLException {
+        Connection con = DBContext.getConnection();
+        PreparedStatement stm = con.prepareStatement("insert OrderHeader values(?,?,?,?)");
+        stm.setInt(1, orh.getId());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        stm.setString(2, sdf.format(orh.getDate()));
+        stm.setString(3, orh.getStatus());
+        stm.setInt(4, orh.getCustomerId());
+        int count = stm.executeUpdate();
+        con.close();
+    }
+
+    public orderheader read(String id) throws SQLException {
+        orderheader orh = null;
+        //Tạo connection để kết nối vào DBMS
+        Connection con = DBContext.getConnection();
+        //Tạo đối tượng PreparedStatement
+        PreparedStatement stm = con.prepareStatement("select * from OrderHeader where id = ?");
+        //Thực thi lệnh SELECT
+        stm.setString(1, id);
+        ResultSet rs = stm.executeQuery();
+        if (rs.next()) {
+            orh = new orderheader();
+            orh.setId(rs.getInt("id"));
+            orh.setDate(rs.getDate("date"));
+            orh.setStatus(rs.getString("status"));
+            orh.setCustomerId(rs.getInt("customerId"));
+        }
+        con.close();
+        return orh;
+    }
+
+    public void update(orderheader orh) throws SQLException {
+        Connection con = DBContext.getConnection();
+        PreparedStatement stm = con.prepareStatement("update OrderHeader set date = ?, status = ?, customerId = ? where id = ?");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        stm.setString(1, sdf.format(orh.getDate()));
+        stm.setString(2, orh.getStatus());
+        stm.setInt(3, orh.getCustomerId());
+        stm.setInt(4, orh.getId());
+        int count = stm.executeUpdate();
+        con.close();
+    }
+
+    public void delete(String id) throws SQLException {
+        Connection con = DBContext.getConnection();
+        // Prepare -> thay đổi dữ liệu
+        PreparedStatement stm = con.prepareStatement("delete from OrderHeader where id = ?");
+        stm.setString(1, id);
+        int count = stm.executeUpdate();
+        con.close();
+    }
+}
